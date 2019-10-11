2ee75f3fa4aed52e1f786f77c2abe084f53306a8
Samer Elkhalid <samer.elkhalid@okstate.edu>
2019-10-10 20:48:59 -0500

    Added Collision detection against vertical movement.

--- Assets/2DPlatformer/Scripts/PhysicsObject.cs
+++ Assets/2DPlatformer/Scripts/PhysicsObject.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class PhysicsObject : MonoBehaviour
 {
+    public float minGroundNormalY = 0.65f;
     public float gravityModifier = 1f;
 
+    protected bool isGrounded;
+    protected Vector2 groundNormal;
     protected Rigidbody2D rigBody2d;
     protected Vector2 velocity;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16);
+
+    protected const float minMoveDistance = 0.001f;
+    protected const float shellRadius = 0.01f;
 
     private void OnEnable()
     {
@@ -17,7 +26,9 @@ public class PhysicsObject : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+        contactFilter.useLayerMask = true;
     }
 
     // Update is called once per frame
@@ -29,14 +40,53 @@ public class PhysicsObject : MonoBehaviour
     private void FixedUpdate()
     {
         velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+
+        isGrounded = false;
+
         Vector2 deltaPos = velocity * Time.deltaTime;
         Vector2 move = Vector2.up * deltaPos.y;
 
-        Movement(move);
+        Movement(move, true);
     }
 
-    private void Movement(Vector2 move)
+    private void Movement(Vector2 move, bool yMovement)
     {
-        rigBody2d.position += move;
+        float distance = move.magnitude;
+
+        if (distance > minMoveDistance)
+        {
+            int count = rigBody2d.Cast(move, contactFilter, hitBuffer, distance + shellRadius);
+            hitBufferList.Clear();
+
+            for (int i = 0; i < count; i++)
+            {
+                hitBufferList.Add(hitBuffer[i]);
+            }
+
+            for (int i = 0; i < hitBufferList.Count; i++)
+            {
+                Vector2 curNormal = hitBufferList[i].normal;
+                if (curNormal.y > minGroundNormalY)
+                {
+                    isGrounded = true;
+                    if (yMovement)
+                    {
+                        groundNormal = curNormal;
+                        curNormal.x = 0;
+                    }
+                }
+
+                float projection = Vector2.Dot(velocity, curNormal);
+                if (projection < 0f)
+                {
+                    velocity -= projection * curNormal;
+                }
+
+                float modifiedDistance = hitBufferList[i].distance - shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+        }
+
+        rigBody2d.position += move.normalized * distance;
     }
 }
